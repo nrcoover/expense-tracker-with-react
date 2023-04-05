9ff841b22e873d00231a5e4cbef8e21205e8769a
nrcoover <81087345+nrcoover@users.noreply.github.com>
2023-04-05 13:51:42 -0400

    creates new expense item component
    dummy data is used as consts in place of future dynamic data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <p>Howdy, Partner!</p>
-    </div>
+    <>
+      <div>
+        <h2>Let's get started!</h2>
+        <p>Howdy, Partner!</p>
+      </div>
+      <ExpenseItem></ExpenseItem>
+    </>
   );
 }
 
