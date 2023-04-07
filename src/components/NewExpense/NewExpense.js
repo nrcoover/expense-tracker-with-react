7dd751b9c6571697679ebfec561e689b98bd1ef4
nrcoover <81087345+nrcoover@users.noreply.github.com>
2023-04-07 11:51:46 -0400

    adds a new expense input form and manages state with useState()

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import "./ExpenseForm.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
