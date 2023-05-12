6b292e90c311bc2016614037f314f3bab62db4ec
nrcoover <81087345+nrcoover@users.noreply.github.com>
2023-05-12 09:36:22 -0400

    replaces static expense items with dynamic array list
    Utilized map() array method to dynamically add new expense item components to the list.

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -16,27 +16,16 @@ const ExpensesList = (props) => {
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter selectedYear={filteredYear} onChangeSelectedYear={changeOfSelectedYearHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpenseFilter 
+          selectedYear={filteredYear} 
+          onChangeSelectedYear={changeOfSelectedYearHandler} />
+        {props.items.map((expense) => ( 
+          <ExpenseItem 
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date} 
+          />
+        ))}
       </Card>
     </div>
     
