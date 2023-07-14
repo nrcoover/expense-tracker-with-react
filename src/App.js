0d1de09aad928791afa0a4c3392b83ba6da6714d
nrcoover <81087345+nrcoover@users.noreply.github.com>
2023-07-14 10:33:13 -0400

    refactors list of expenses to update dynamically

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React from "react";
+import { React, useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpensesList from "./components/Expenses/ExpensesList";
 
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [ expenses, setExpenses ] = useState(INITIAL_EXPENSES)
 
   //   return React.createElement(
   //     'div',
@@ -35,8 +37,9 @@ const App = () => {
   // }
 
   const addExpenseItemHandler = (expenseItem) => {
-    console.log("In App.js");
-    console.log(expenseItem)
+    setExpenses((prevExpenses) => {
+      return [expenseItem, ...prevExpenses]
+    });
   }
 
   return (
