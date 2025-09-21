57b02bb9bba377938416ec6ba62be3114278d127
nicklefritz <nrcoover@gmail.com>
2025-09-21 15:23:12 -0400

    Fix char display bug

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -5,7 +5,7 @@ import './Chart.css';
 
 const Chart = props => {
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
-  const totalMaximum = Math.max(dataPointValues);
+  const totalMaximum = Math.max(...dataPointValues);
   
   return <div className="chart">
     {props.dataPoints.map(dataPoint => 
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './ChartBar.css'
 
 const ChartBar = props => {
-    const barFillHeight = '0%s'
+    let barFillHeight = '0'
     
     if (props.maxValue > 0) {
         barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
